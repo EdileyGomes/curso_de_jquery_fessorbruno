59ff4acd565f2e0657e141a9fae80635f3106a3d
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-02 10:02:56 -0400

    Update script.js

--- Aula-04-Eventos-em-jQuery/Aula-04-b/script/script.js
+++ Aula-04-Eventos-em-jQuery/Aula-04-b/script/script.js
@@ -1,14 +1,5 @@
 $(document).ready(()=>{
-    $('#bloco').click(function() { 
-        $('#texto').html('Olá, Mundo!')
-    }); 
-    $('#bloco').mouseenter(function() { 
-        $('#texto').html('Mouse sobre o quadrado')
-    });
-    $('#bloco').mouseleave(function() { 
-        $('#texto').html('Mouse saiu')
-    });
-    $('#bloco').mousemove(function() { 
-        $('#texto').html('Mouse moveu no quadrado')  
+    $('#nome').keyup(function (e) { 
+        $('#texto').html(`${nome.value}`)
     });
 });
