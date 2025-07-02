7afc6ea45fc7eebc0fdbff53c114e5730567e6fe
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-01 22:16:08 -0400

    Add sum functionality using JS and jQuery
    Implemented sum calculation for two input values using both vanilla JavaScript and jQuery. The JavaScript logic was added to script.js, and the jQuery logic was added inline in the HTML file, triggered by respective buttons.

---
+++ Aula-02-Primeiros-Códigos-com-jQuery/script/script.js
@@ -0,0 +1,13 @@
+let btn_somar = document.getElementById('btn-somarjs').addEventListener('click', function() {
+    let v1 = document.getElementById('v1').value
+    let v2 = document.getElementById('v2').value
+
+    soma = parseInt(v1) + parseInt(v2)
+
+    let res = document.getElementById('res').value = soma
+
+    // jquery
+
+    jqu
+
+})
