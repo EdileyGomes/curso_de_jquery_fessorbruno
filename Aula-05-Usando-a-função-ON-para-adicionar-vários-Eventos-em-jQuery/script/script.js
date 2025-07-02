53b78c7be1cd4926b358535b73a072e5cb62717f
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-02 10:03:04 -0400

    Create script.js

---
+++ Aula-05-Usando-a-função-ON-para-adicionar-vários-Eventos-em-jQuery/script/script.js
@@ -0,0 +1,17 @@
+$(document).ready(()=>{
+    $('.bloco').on({
+        click: function(){
+            $('#texto').html('Mouse clicou')
+        },
+        mouseenter: function() {
+            $('#texto').html('Mouse entrou no quadrado!')
+        },
+        mouseleave: function() {
+            $('#texto').html('Mouse saiu do Quadrado')
+        },
+        dblclick: function() {
+            alert('Clicou duas vezes')
+        }
+    })
+});
+    
