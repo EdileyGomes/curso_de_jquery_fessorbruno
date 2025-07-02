15e1965a91afc88a6c1e0609594fb80cd11c4447
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-02 09:15:39 -0400

    Update jQuery events for #bloco element
    Replaced previous button click and slide logic with mouse event handlers for the #bloco element. Now updates #texto content based on click, mouseenter, mouseleave, and mousemove events.

--- Aula-04-Eventos-em-jQuery/script/script.js
+++ Aula-04-Eventos-em-jQuery/script/script.js
@@ -1,13 +1,14 @@
 $(document).ready(()=>{
-    //$('#texto').html('<input type="text" name="" id="" placeholder="Olá Mundo">')
-    $('#btn_clicar').click(function() {
-        $('#btn_recolher').css('display', 'flex').slideDown(300)
-        $('#texto').slideDown(300)
-        $('#texto').html('<input type="text" name="" id="" placeholder="Olá Mundo">').css('display', 'flex')
-    })
-
-    $('#btn_recolher').click( function() {
-        $('#texto').slideUp(300)
-        $('#btn_recolher').slideUp(300)
-    })
-})
+    $('#bloco').click(function() { 
+        $('#texto').html('Olá, Mundo!')
+    }); 
+    $('#bloco').mouseenter(function() { 
+        $('#texto').html('Mouse sobre o quadrado')
+    });
+    $('#bloco').mouseleave(function() { 
+        $('#texto').html('Mouse saiu')
+    });
+    $('#bloco').mousemove(function() { 
+        $('#texto').html('Mouse moveu no quadrado')  
+    });
+});
