d37c1500313e6333376c623d3c6a18fad696b887
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-02 10:03:15 -0400

    Add jQuery event trigger demo with two blocks
    Introduces an HTML page and JavaScript to demonstrate dynamic event triggering in jQuery. The first block responds to mouse events and the second block triggers a click event on the first block when clicked.

---
+++ Aula-06-jQuery-trigger-acionando-eventos-de-forma-dinâmica/script/script.js
@@ -0,0 +1,21 @@
+$(document).ready(()=>{
+    $('.bloco').on({
+        click: function(){
+            $('#texto').html('Mouse clicou')
+        },
+        mouseenter: function() {
+            $('#texto').html('Mouse entrou no quadrado!')
+        },
+        mouseleave: function() {
+            $('#texto').html('Mouse saiu do Quadrado')
+        },
+        dblclick: function() {
+            alert('Clicou duas vezes')
+        }
+    })
+
+    $(".bloco2").click(function() {
+        $('.bloco').trigger('click')
+    });
+});
+    
