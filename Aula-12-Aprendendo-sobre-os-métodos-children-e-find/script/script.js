74b62bf8247387c855b1861fdbdb5ce250369699
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-03 09:38:46 -0400

    Add script demonstrating jQuery children and find methods
    Introduces a script that applies a border to all elements using jQuery's find method. Also includes commented code for toggling classes and using the children method for educational purposes.

---
+++ Aula-12-Aprendendo-sobre-os-métodos-children-e-find/script/script.js
@@ -0,0 +1,14 @@
+/*$('div').children().css({'border':'1px solid red'})
+
+$('.li1').toggleClass('.li2');
+
+
+$('button').click(function (e) { 
+    e.preventDefault();
+    e.stopPropagation()
+
+    $('.li1').toggleClass('li2');
+});
+*/
+
+$('html').find('*').css({'border':'1px solid black'})
