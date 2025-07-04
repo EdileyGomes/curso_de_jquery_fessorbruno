ca2f9cc6a77da36953cc623cd2085d8230d24f02
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-04 17:41:48 -0400

    Update script.js

--- Aula-03-As-formas-corretas-de-inserir-o-código-jQuery/script/script.js
+++ Aula-03-As-formas-corretas-de-inserir-o-código-jQuery/script/script.js
@@ -1,3 +1,18 @@
+$(document).ready(function () {
+    $('#btn_clicar').click(() => { 
+        
+        $('#btn_recolher').css('display', 'flex').slideDown(300)
+        $('#texto').html(`Olá, Mundo!`).css('display', 'flex').slideDown(300);
+    });
+
+    $('#bnt_recolher').click(function (e) { 
+        e.preventDefault();
+        
+        $('#btn_recolher').css('display', 'flex').slideUp(300)
+        $('#texto').html(`Olá, Mundo!`).css('display', 'flex').slideUp(300);
+    });
+});
+
 $(document).ready(()=>{
     //$('#texto').html('<input type="text" name="" id="" placeholder="Olá Mundo">')
     $('#btn_clicar').click(function() {
