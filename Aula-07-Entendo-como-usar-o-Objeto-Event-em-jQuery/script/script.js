47ccbe8acd27643ee50365e717aba7272c59550b
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-05 10:28:29 -0400

    Update script.js

--- Aula-07-Entendo-como-usar-o-Objeto-Event-em-jQuery/script/script.js
+++ Aula-07-Entendo-como-usar-o-Objeto-Event-em-jQuery/script/script.js
@@ -1,3 +1,19 @@
-$(document).mousemove((event) => {
-    $('#texto').html(`Mouse X: ${event.pageX}px <br> Mouse Y: ${event.pageY}px`)
+let pmX = 0
+let pmY = 0
+
+$(document).ready(function () {
+    $(document).mousemove(function (e) { 
+        let pmX = e.pageX
+        let pmY = e.pageY
+        $('#texto').html(`Valor do X: ${e.pageX} <br /> Valor do Y: ${e.pageY}`);
+
+        console.log(`X: ${pmX}`)
+        console.log(`Y: ${pmY}`)
+
+        if (e.pageY < 500) {
+            $('#texto2').html(`Está abaixo de 500`);
+        } else {
+            $('#texto2').html(`Está acima de 500`);
+        }
+    });
 });
