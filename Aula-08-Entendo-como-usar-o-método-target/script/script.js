091c37ffe2bdb5f7443d6526835e237614e02edf
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-05 10:31:20 -0400

    Olá, Mundo

--- Aula-08-Entendo-como-usar-o-método-target/script/script.js
+++
@@ -1,6 +0,0 @@
-$(document).click((event) => { 
-    $('#resultado').html(`Objeto clicado: ${event.target.id}`)
-    if (event.target.id == 'resultado') {
-        console.log('Parabenssssssss')
-    }
-});
