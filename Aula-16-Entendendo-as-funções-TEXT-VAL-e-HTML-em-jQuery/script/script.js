77f5ccbde2e0ff189df14f2d28a37779c11043bb
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-08 20:52:41 -0400

    Add example using jQuery filter and not functions
    Introduces an HTML page and a script demonstrating the use of jQuery's .filter() and .not() methods to style <p> elements with and without the 'borda' class.

---
+++ Aula-16-Entendendo-as-funções-TEXT-VAL-e-HTML-em-jQuery/script/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    $('p').filter('.borda').css({'background-color':'#00f','color':'white'})
+    $('p').not('.borda').css({'background-color':'#f0f','color':'white'})
+});
