469026a18131cb8b50fafabc707d8816d4297a9d
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-08 22:09:27 -0400

    Aulão super reforçado do fessor bruno. Commit feito pelo terminal

--- Aula-16-Entendendo-as-funções-TEXT-VAL-e-HTML-em-jQuery/script/script.js
+++ Aula-16-Entendendo-as-funções-TEXT-VAL-e-HTML-em-jQuery/script/script.js
@@ -1,4 +1,3 @@
-$(document).ready(function () {
-    $('p').filter('.borda').css({'background-color':'#00f','color':'white'})
-    $('p').not('.borda').css({'background-color':'#f0f','color':'white'})
-});
+$('#caixa').click(function() {
+    alert(`Texto ${$(this).html()}`)
+})
