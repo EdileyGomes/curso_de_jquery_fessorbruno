e2ab1e50967e0f9e1ad7fc533b4afe1bfa861f9d
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-08 20:24:34 -0400

    Update script.js

--- Teste-02/script/script.js
+++ Teste-02/script/script.js
@@ -1,10 +1,26 @@
 $(document).ready(function () {
-    $('p').first().css('border','5px solid violet')
-    $('p').last().css('border','5px solid aquamarine')
+    let cont = 0 
+    let filhosDiv = $('#div1').children().length
+    $('#button1').click(function (e) { 
+        e.preventDefault();
+        e.stopPropagation()
+        
+        if (cont < filhosDiv) {
+            e.stopPropagation()
+            $('#div1').children().css('border','none')
+            $('#div1').children().eq(cont).css('border','5px solid red')
 
-    $('h3').first().css('border','5px solid violet')
-    $('h3').last().css('border','5px solid aquamarine')
-
-    $('p').eq(2).css('border','10px solid red')
-    $('h3').eq(2).css('border','10px solid red')
+            cont ++
+            console.log(cont)
+        }
+        if (cont >= filhosDiv) {
+            cont = 0
+        }
+    });
+    $('#button2').click(function (e) { 
+            e.preventDefault();
+            e.stopPropagation()
+            
+            alert(`O Contador é o ${cont}, e seu texto é: ${$('#div1').children().eq(cont - 1).text()}`)
+    });
 });
