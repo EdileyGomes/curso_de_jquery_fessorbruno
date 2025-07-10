c46f35b2676cf18ad157f19b42faa75d83b1f1f6
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-09 21:45:23 -0400

    Update script.js

--- Aula-16-Entendendo-as-funções-TEXT-VAL-e-HTML-em-jQuery/script/script.js
+++ Aula-16-Entendendo-as-funções-TEXT-VAL-e-HTML-em-jQuery/script/script.js
@@ -1,3 +1,17 @@
-$('#caixa').click(function() {
-    alert(`Texto ${$(this).html()}`)
+$('h2').click(function (e) { 
+    e.preventDefault();
+    
+    
+
+    alert(`Você clicou no ${$(this).html()}`)
+});
+
+$('#caixa').click(function (e) { 
+    e.preventDefault();
+    
+    alert(`Você clicou no ${$(this).html()}`)
+});
+
+$('#nome').keyup(() => {
+    $('#sla').text(`Você digitou: ${$('#nome').val()}`);
 })
