c59b903f18833416cc34c4b49fe7a15ec1787ed9
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-10 20:06:42 -0400

    Update script.js

--- Aula-17-Obter-e-modificar-valores-dos-atributos-dos-elementos-HTML/script/script.js
+++ Aula-17-Obter-e-modificar-valores-dos-atributos-dos-elementos-HTML/script/script.js
@@ -1,20 +1,19 @@
-$('#caixa').click(function (e){
-    e.preventDefault()
+cont = 0
+$('#caixa').click(function (e) {
+    
+    if (cont == 0) {
+        $(this).attr('class', 'azul')
 
-    $(this).attr('class', 'azul')
-    $(this).attr()
+        cont ++
+    } else {
+        $(this).attr('class', 'vermelho')
+        cont = 0
+    }
 })
 
-$('#btn-vermelho').click(function (e) { 
-    e.preventDefault();
-    
-    $('#caixa').css('color', 'black')
-    $("#caixa").attr('class', 'vermelho')
-});
-
-$('#btn-azul').click(function (e) { 
-    e.preventDefault();
-    
-    $('#caixa').css('color', 'white')
-    $("#caixa").attr('class', 'azul')
-});
+$('#btn-azul').click(function() {
+    $('iframe').attr('src','https://www.youtube.com/embed/WiKsK0kmzlU?si=2QbzF2NfvrswHexU')
+})
+$("#btn-vermelho").click(function() {
+    $('iframe').attr('src','https://www.youtube.com/embed/Homo-ORr-9o?si=azoCxCitm5MGDc6o')
+})
