b3d2fe5ea53e979555aaa3c29bdef46067e16fdd
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-10 21:14:05 -0400

    Update script.js

--- Aula-20-Aprendendo-sobre-a-Função-ANIMATE-em-jQuery/script/script.js
+++ Aula-20-Aprendendo-sobre-a-Função-ANIMATE-em-jQuery/script/script.js
@@ -1,11 +1,29 @@
-$('#btn-tggle').click(function (e) { 
+$('#aumentar').click(function (e) { 
     e.preventDefault();
     
-    $('#caixa').toggle(500, function() {
-        if ($(this).is(':visible')){
-            $('#btn-tggle').text('Ocultar');
-        } else {
-            $('#btn-tggle').text('Mostrar');
-        }
-    })
+    $('#caixa, #caixa2').animate({width:"500px", height:'500px'})
+});
+
+$('#diminuir').click(function (e) { 
+    e.preventDefault();
+    
+    $('#caixa, #caixa2').animate({width:"200px"}).animate({height:'200px'},{duration: 1000})
+});
+
+$('#direira').click(function (e) { 
+    e.preventDefault();
+    e.stopPropagation();
+    
+    $('#caixa').animate({left:'500px'},{duration:200, complete: function() {
+        alert('Animação concluída para a direita!');
+    }} )
+});
+
+$('#esquerda').click(function (e) { 
+    e.preventDefault();
+    e.stopPropagation();
+    
+    $('#caixa').animate({left:'0px'},{duration:1000, complete: function() {
+        alert('Animação concluída para a Esquerda!');
+    }} )
 });
