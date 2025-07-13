406395c5e5952cf1099966c9f17e508a996e3051
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-13 17:02:44 -0400

    Update script.js

--- Aula-24-Aprendendo-sobre-a-função-fadeTO-em-jQuery/script/script.js
+++ Aula-24-Aprendendo-sobre-a-função-fadeTO-em-jQuery/script/script.js
@@ -1,9 +1,9 @@
-$("#menu").children().on(
-    'mouseenter', function (){
-        $(this).siblings().fadeTo(500, 0.5);
-    
-})
 
-$("#menu").children().on("mouseleave", function () {
-    $(this).siblings().fadeTo(200, 1);
-});
+$("#menu").children().on({
+    mouseenter: function() {
+        $(this).fadeTo(500, 0.5);
+    },
+    mouseleave: function() {
+        $(this).fadeTo(500, 1);
+    },
+})
