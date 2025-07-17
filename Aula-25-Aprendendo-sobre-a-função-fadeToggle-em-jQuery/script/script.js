cb8bd817187ec92a141245dfac9b48e4e5a244cb
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-16 22:02:54 -0400

    Update script.js

--- Aula-25-Aprendendo-sobre-a-função-fadeToggle-em-jQuery/script/script.js
+++ Aula-25-Aprendendo-sobre-a-função-fadeToggle-em-jQuery/script/script.js
@@ -6,11 +6,27 @@ $('.itemMenu').mouseover(function () {
 });
 
 $('.itemMenu').mouseleave(function () { 
-    $(this).siblings().fadeTo(100, 1);
+    $(this).siblings().fadeTo(0 ,1);
     $(this).css({'transform': 'scale(1)'});
 });
 
 
 $("#menujQuery").click(function () {
-    $("#subMenujQuery").fadeToggle(1000)
+    $("#subMenujQuery").fadeToggle(500)
+    $("#subMenujQuery").css('display', 'flex');
+})
+
+$("#menuCanvas").click(function () {
+    $("#subMenuCanvas").fadeToggle(500)
+    $("#subMenuCanvas").css('display', 'flex');
+})
+
+$("#menuPython").click(function () {
+    $("#subMenuPython").fadeToggle(500)
+    $("#subMenuPython").css('display', 'flex');
+})
+
+$("#menuArduino").click(function () {
+    $("#subMenuArduino").fadeToggle(500)
+    $("#subMenuArduino").css('display', 'flex');
 })
