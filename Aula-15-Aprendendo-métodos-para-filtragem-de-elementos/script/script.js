cc05574db932029b911e10756bdc63e46b15bbee
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-23 21:30:54 -0400

    Remove jQuery filtering and styling script
    Deleted the script that applied different background and text colors to <p> elements based on the presence of the 'borda' class. This may be in preparation for a new implementation or refactoring.

--- Aula-15-Aprendendo-métodos-para-filtragem-de-elementos/script/script.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function () {
-    $('p').filter('.borda').css({'background-color':'#00f','color':'white'})
-    $('p').not('.borda').css({'background-color':'#f0f','color':'white'})
-});
