2efdf422feb18685f1c953074a7064dfb8722955
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-25 21:21:55 -0400

    Add toggle animation and alert on button click
    Assigned IDs to <h3> and <p> elements for targeted animation. Updated the script to animate opacity toggle on the container and width/height toggle on the new elements, displaying an alert when the animation completes.

--- Aula-21-Aprendendo-sobre-TOGGLE-na-Função-ANIMATE-em-jQuery/script/script.js
+++ Aula-21-Aprendendo-sobre-TOGGLE-na-Função-ANIMATE-em-jQuery/script/script.js
@@ -4,8 +4,14 @@ $('#btn-tamanho').click(function (e) {
     $('#caixa').animate({width:'toggle', height:'toggle'})
 });
 
+
 $('#btn-mostrar').click(function (e) { 
     e.preventDefault();
     
-    $('#caixa').animate({})
+    function alerta() {
+        alert('Animação Acabada')
+    }
+
+    $('#caixa').animate({opacity:'toggle'})
+    $('#sla').animate({width:"toggle", height:"toggle"}, 500, alerta)
 });
