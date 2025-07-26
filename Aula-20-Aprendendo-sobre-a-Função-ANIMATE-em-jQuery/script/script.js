949fa85b73236db42f211b09caed716db8b5cfd4
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-25 21:01:12 -0400

    Update script.js

--- Aula-20-Aprendendo-sobre-a-Função-ANIMATE-em-jQuery/script/script.js
+++ Aula-20-Aprendendo-sobre-a-Função-ANIMATE-em-jQuery/script/script.js
@@ -1,15 +1,19 @@
 $(document).ready(function evento() {
-    let numH = $('#numeroH').val()
-    let numW = $('#numeroH').val()
-
     $('#aumentar').click(function (e) { 
         e.preventDefault();
     
         $('#caixa').animate({width:'500px', height: '500px'})
     });
 
-    function animacao() {
-        $('#caixa2').animate({height:`${numH}px`}).delay(1000).animate({width:`${numW}px`},{duration:500, animacao})
-    }
-    animacao()
+    $('#direita').click(function (e) { 
+        e.preventDefault();
+        
+        let numH = $('#numeroH').val()
+        let numW = $('#numeroW').val()
+
+        function numero() {
+            $('#caixa2').animate({height:`${numH}px`}).delay(100).animate({width:`${numW}px`},{duration:500, numero})
+        }
+        numero()
+    });
 });
