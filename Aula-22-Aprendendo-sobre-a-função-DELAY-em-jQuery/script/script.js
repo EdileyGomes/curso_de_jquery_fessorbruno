9cf59d185a5a85f69ea2581f53956a2a17b30c94
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-26 18:53:32 -0400

    Certinho minha rapaziada

--- Aula-22-Aprendendo-sobre-a-função-DELAY-em-jQuery/script/script.js
+++ Aula-22-Aprendendo-sobre-a-função-DELAY-em-jQuery/script/script.js
@@ -1,14 +1,22 @@
+function sla() {
+    $("p").text("Eu Sou Ediley Gomes!")
+}
+
 $('#btn-aumentar').click(function (e) { 
     e.preventDefault();
     
+    $('#caixa').animate({width:'400px', height:'400px'}).delay(200).animate({width:"200px", height:"200px"},1000, sla)
 });
 
 $('#btn-diminuir').click(function (e) { 
     e.preventDefault();
     
+    $('#caixa').animate({width:'200px', height:'200px'})
 });
 
 $('#btn-mostrar').click(function (e) { 
     e.preventDefault();
     
+    $('#caixa').delay(1000).fadeToggle()
 });
+
