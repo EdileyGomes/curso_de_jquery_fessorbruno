91c639662bbe20cd82b708db7ff0e97905ce0371
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-28 21:19:41 -0400

    Update script.js

--- Aula-25-Aprendendo-sobre-a-função-fadeToggle-em-jQuery/script/script.js
+++ Aula-25-Aprendendo-sobre-a-função-fadeToggle-em-jQuery/script/script.js
@@ -11,6 +11,68 @@ $('.itemMenu').mouseleave(function () {
 $('#menujQuery').click(function (e) { 
     e.preventDefault();
     
-    $('#subMenujQuery').fadeToggle()
-    $('#subMenujQuery')
+    if ($('#subMenuCanvas').is(':visible') || $('#subMenuPython').is(':visible') || $('#subMenuArduino').is(':visible')) {
+        $('#subMenuCanvas').fadeOut()
+        $('#subMenuPython').fadeOut()
+        $('#subMenuArduino').fadeOut()
+
+        $('#subMenujQuery').fadeToggle()
+        $('#subMenujQuery').css({display:"flex"})
+
+    } else {
+        $('#subMenujQuery').fadeToggle()
+        $('#subMenujQuery').css({display:"flex"})
+    }
+});
+
+$('#menuCanvas').click(function (e) { 
+    e.preventDefault();
+    
+    if ($('#subMenujQuery').is(':visible') || $('#subMenuPython').is(':visible') || $('#subMenuArduino').is(':visible')) {
+        $('#subMenujQuery').fadeOut()
+        $('#subMenuPython').fadeOut()
+        $('#subMenuArduino').fadeOut()
+
+        $('#subMenuCanvas').fadeToggle()
+        $('#subMenuCanvas').css({display:"flex"})
+
+    } else {
+        $('#subMenuCanvas').fadeToggle()
+        $('#subMenuCanvas').css({display:"flex"})
+    }
+    
+});
+
+$('#menuPython').click(function (e) { 
+    e.preventDefault();
+    
+    if ($('#subMenuCanvas').is(':visible') || $('#subMenujQuery').is(':visible') || $('#subMenuArduino').is(':visible')) {
+        $('#subMenuCanvas').fadeOut()
+        $('#subMenujQuery').fadeOut()
+        $('#subMenuArduino').fadeOut()
+
+        $('#subMenuPython').fadeToggle()
+        $('#subMenuPython').css({display:"flex"})
+
+    } else {
+        $('#subMenuPython').fadeToggle()
+        $('#subMenuPython').css({display:"flex"})
+    }
+});
+
+$('#menuArduino').click(function (e) { 
+    e.preventDefault();
+    
+    if ($('#subMenuCanvas').is(':visible') || $('#subMenuPython').is(':visible') || $('#subMenuArduino').is(':visible')) {
+        $('#subMenuCanvas').fadeOut().finish()
+        $('#subMenuPython').fadeOut()
+        $('#subMenujQuery').fadeOut().finish()
+
+        $('#subMenuArduino').fadeToggle()
+        $('#subMenuArduino').css({display:"flex"})
+
+    } else {
+        $('#subMenuArduino').fadeToggle()
+        $('#subMenuArduino').css({display:"flex"})
+    }
 });
