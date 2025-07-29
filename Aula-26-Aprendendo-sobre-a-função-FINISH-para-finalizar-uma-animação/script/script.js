787945e4e7bef349a555fd75c4c5d3217c95a10c
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-29 19:56:54 -0400

    Update script.js

--- Aula-26-Aprendendo-sobre-a-função-FINISH-para-finalizar-uma-animação/script/script.js
+++ Aula-26-Aprendendo-sobre-a-função-FINISH-para-finalizar-uma-animação/script/script.js
@@ -1,34 +1,29 @@
 $('#aumentar').click(function (e) { 
     e.preventDefault();
-
-
-    $('.caixa').animate({width:"800px"}, 1500 )
+    
+    $(".caixa").animate({width:"500px"},{duration:1000})
 });
 
 $('#diminuir').click(function (e) { 
     e.preventDefault();
-
-    $('.caixa').animate({width:"100px"}, 1500 )
+    
+    $(".caixa").animate({width:"100px"},{duration:1000})
 });
 
 $('#finalizar').click(function (e) { 
     e.preventDefault();
     
-
-    $('.caixa').finish()
+    $('.caixa').finish() 
 });
 
-let cont = 0
+function sla() {
+    alert("Olá?")
+}
 
-$('#stop').click(function (e){
-    e.preventDefault()
+sla()
 
-    $('.caixa').stop()
-    if (cont === 0) {
-        cont = 1
-        $(".caixa").html('<br><br><br><br><br><br><p>a animação parou</p>')
-    } else {
-        cont = 0
-        $(".caixa p").remove()
-    }
-})
+$('#stop').click(function (e) { 
+    e.preventDefault();
+    
+    $('.caixa').stop()  
+});
