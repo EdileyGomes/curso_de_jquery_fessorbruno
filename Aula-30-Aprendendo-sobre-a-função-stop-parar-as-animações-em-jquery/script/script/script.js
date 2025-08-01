7bd8d45e7025dbe971e01dad5cd3df7588f6ca2d
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-31 21:20:37 -0400

    Update script.js

--- Aula-30-Aprendendo-sobre-a-função-stop-parar-as-animações-em-jquery/script/script/script.js
+++ Aula-30-Aprendendo-sobre-a-função-stop-parar-as-animações-em-jquery/script/script/script.js
@@ -1,19 +1,29 @@
+const cx = $('#caixa')
+
 $('#btn-animar').click(function (e) { 
     e.preventDefault();
     
+    cx.animate({left:'800px'},{duration:2000})
+    cx.animate({top:'400px'},{duration:2000})
+    cx.animate({left:'00px'},{duration:2000})
+    cx.animate({top:'50px'},{duration:2000})
 });
 
 $('#btn-proxima').click(function (e) { 
     e.preventDefault();
     
+
+    cx.stop(false, false)// limpar fila, segunda parametro é pular para o fim
 });
 
 $('#btn-parar').click(function (e) { 
     e.preventDefault();
     
+    cx.stop(true, false)
 });
 
 $('#btn-fim').click(function (e) { 
     e.preventDefault();
     
+    cx.stop(false, true)
 });
