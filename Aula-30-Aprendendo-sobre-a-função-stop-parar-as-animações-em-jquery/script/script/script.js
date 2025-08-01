c64de929b9438a641d0fd7ce415c63544cf6f056
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-31 21:08:43 -0400

    Update script.js

---
+++ Aula-30-Aprendendo-sobre-a-função-stop-parar-as-animações-em-jquery/script/script/script.js
@@ -0,0 +1,19 @@
+$('#btn-animar').click(function (e) { 
+    e.preventDefault();
+    
+});
+
+$('#btn-proxima').click(function (e) { 
+    e.preventDefault();
+    
+});
+
+$('#btn-parar').click(function (e) { 
+    e.preventDefault();
+    
+});
+
+$('#btn-fim').click(function (e) { 
+    e.preventDefault();
+    
+});
