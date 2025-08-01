ee37e137d46ac25f27ec8166382334c11f767ae2
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-07-31 21:45:46 -0400

    Update script.js

--- Aula-31-Aprendendo-sobre-as-funções-Append-e-AppendTo-em-jQuery/script/script.js
+++ Aula-31-Aprendendo-sobre-as-funções-Append-e-AppendTo-em-jQuery/script/script.js
@@ -6,9 +6,9 @@ $('#btn-adicionar').click(function (e) {
     r = Math.floor(Math.random()*255)
     g = Math.floor(Math.random()*255)
     b = Math.floor(Math.random()*255)
-    a = Math.floor(Math.random()*1)
+    a = Number(Math.random().toFixed(3));
 
-    $('#caixa').append(`<div class='subcaixa' style='background-color:rgb(${r},${g},${b})'> </div>`);
+    $('#caixa').append(`<div class='subcaixa' style='background-color:rgba(${r},${g},${b},${a})'> </div>`);
 });
 
 $('#btn-adicionar5').click(function (e) { 
@@ -18,9 +18,9 @@ $('#btn-adicionar5').click(function (e) {
         r = Math.floor(Math.random()*255)
         g = Math.floor(Math.random()*255)
         b = Math.floor(Math.random()*255)
-        a = Math.floor(Math.random()*1)
+        a = Number(Math.random().toFixed(3));
 
-        $('#caixa').append(`<div class='subcaixa' style='background-color:rgb(${r},${g},${b})'> </div>`);
+        $('#caixa').append(`<div class='subcaixa' style='background-color:rgba(${r},${g},${b},${a})'> </div>`);
     }
     
 });
