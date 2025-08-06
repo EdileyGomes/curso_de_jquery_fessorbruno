e01722591ae4684814d293d54061ca8e87c45d5f
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-08-05 20:35:49 -0400

    Update script.js

--- Aula-34-Removendo-elementos-com-as-funções-Remove-e-Empty-em-jQuery/script/script.js
+++ Aula-34-Removendo-elementos-com-as-funções-Remove-e-Empty-em-jQuery/script/script.js
@@ -1,6 +1,6 @@
 let r,g,b
 let num = 1
-$('#btn-adicionarantes').click(function (e) { 
+$('#btn-adicionar').click(function (e) { 
     e.preventDefault();
     
     r = Math.floor(Math.random()*255)
@@ -8,7 +8,33 @@ $('#btn-adicionarantes').click(function (e) {
     b = Math.floor(Math.random()*255)
     a = Number(Math.random().toFixed(3));
 
-    $('#caixa').before(`<div class='subcaixa' style='background-color:rgba(${r},${g},${b},${a})'>${num}</div>`);
+    $('#caixa').append(`<div id='d${num}' class='subcaixa' style='background-color:rgba(${r},${g},${b},${a})'>${num}</div>`);
 
     num ++
+});
+
+
+$('#btn-remover').click(function (e) { 
+    e.preventDefault();
+
+    let num2 = $('#caixa').children().length
+
+    if (num = num2) {
+        num2 = 0
+
+        $("#d"+(num2 ++)).remove()
+
+        num ++
+    }
+
+    console.log(num2)
+});
+
+$('#btn-removerconteudo').click(function (e) { 
+    e.preventDefault();
+    
+    $('#caixa').empty()
+
+    $('#caixa').append(`<div id='d${num = 0}' class='subcaixa' style='background-color:rgba(${r},${g},${b},${a})'>${num = 0}</div>`);
+    num = 1
 });
