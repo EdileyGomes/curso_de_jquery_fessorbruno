c60a934b4e423b7bc96f3169cc1848813bcff1ce
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-08-07 14:09:13 -0400

    Delete script.js

--- Aula-37-Usando-a-Função-Clone-para-clonar-elementos-em-jQuery/script/script.js
+++
@@ -1,8 +0,0 @@
-$('#btn-clonar').click(function (e) { 
-    e.preventDefault();
-    
-    $("#caixa").clone().appendTo('#caixa1');
-
-    let num = $('#caixa1').children().length; 
-    
-    console.log(num);})
