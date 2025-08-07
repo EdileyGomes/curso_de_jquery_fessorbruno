593ae454403966b62ba2f9acda453c55a3f304d7
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-08-07 14:09:25 -0400

    Delete script.js

--- Aula-36-Função-toggleClass-para-adicionar-e-remove-classe-CSS-pelo-jQuery/script/script.js
+++
@@ -1,9 +0,0 @@
-let alerta = setInterval(() => {
-    $("#caixa").toggleClass('alerta')
-}, 1000)
-
-$('#sla').click(function (e) { 
-    e.preventDefault();
-    
-    $('#caixa2').toggleClass('alerta');
-});
