c247f26fa61925bf9095ec4b1493a44ec6c96139
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-08-18 18:40:46 -0400

    Update script.js

--- Teste-05/script/script.js
+++ Teste-05/script/script.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
                 let resultado = math.evaluate(expressao.replace(/x/g, '*'));
                 $('#valor').val(resultado);
                 expressao = resultado.toString(); // atualiza a expressão com o resultado
-            } catch (err) {
+            } catch (err) {1
                 $('#valor').val('Erro');
                 expressao = '';
             }
