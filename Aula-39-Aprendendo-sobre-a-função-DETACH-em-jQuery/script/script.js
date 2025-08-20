c77ffc5a2a30d7cd03fa30c3b0496f3e81daa239
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-08-20 12:43:34 -0400

    Implement item detach and reset functionality
    Added logic to detach items from caixa1 and append them to caixa2 on button click, using a counter to track the current item. Also implemented a reset button to move all items back to caixa1 and reset the counter.

--- Aula-39-Aprendendo-sobre-a-função-DETACH-em-jQuery/script/script.js
+++ Aula-39-Aprendendo-sobre-a-função-DETACH-em-jQuery/script/script.js
@@ -1,10 +1,25 @@
-let itens = $('.item')
-let indice = 0 
+let itens = $(".item")
+let i = 0 
 
 console.log(itens)
 
 $('#btn-mudar').click(function (e) { 
     e.preventDefault();
     
-    let item1 = 1
+    let item = $(itens[i]).detach()
+
+    i++
+    let num = $("#caixa2 .item").length;
+    $("#caixa2").append(item)
+    
+});
+
+$('#btn-reset').click(function (e) { 
+    e.preventDefault();
+
+    for (let j = 0; j < itens.length; j++) {
+        $("#caixa1").append($(itens[j]));
+    }
+
+    i = 0
 });
