200c0ff795d92d7c5c61f4d01219597e59e78d13
Ediley Gomes <ediley.gomes@pucpr.edu.br>
2025-08-23 10:43:38 -0400

    Mais uma aula do professor bruno, sobre jquery, e nessa aula, aprendi a como usar os métodos removeClass e removeAttr

--- Aula-43-Remover-atributos-e-classes-com-as-funções-removeAttr-e-removeClass/script/script.js
+++ Aula-43-Remover-atributos-e-classes-com-as-funções-removeAttr-e-removeClass/script/script.js
@@ -1,18 +1,62 @@
-$('#btn-posição').click( function(){
-    let pt = $('#caixa').position().top
-    let pl = $("#caixa").position().left
-    let ot = $('#caixa').position().top
-    let ol = $("#caixa").position().left
+$('#btn-removerAtributo').click(function (e) { 
+    e.preventDefault();
 
-    alert("top: " + pt + ", left: " + pl + " || offset top: " + ot + ", offset left: " + ol)
-})
+    $("#caixa").removeAttr('style');
+});
 
-$("#top").change((e) => {
-    let valor = document.getElementById(e.target.id).value
-    $("#caixa").offset({ top: valor})
-})
 
-$("#left").change((e) => {
-    let valor = document.getElementById(e.target.id).value
-    $("#caixa").offset({ left: valor })
-})
+
+$('#btn-removerClasse').click(function (e) { 
+    e.preventDefault();
+
+    console.log("Classe removida");
+
+    $("#caixa").removeClass('borda');
+});
+
+
+$('#btn-adicionarClasse').click(function (e) { 
+    e.preventDefault();
+
+    console.log("Classe adicionada");
+
+    $("#caixa").addClass('borda');
+});
+
+
+
+
+
+
+
+// Pega a posição atual da caixa
+let caixaPos = $("#caixa").position();
+let caixaTop = caixaPos.top;
+let caixaLeft = caixaPos.left;
+
+// Pega os valores dos sliders
+let sliderTop = $("#top").val();
+let sliderLeft = $("#left").val();
+
+console.log("Posição da caixa:", caixaTop, caixaLeft);
+console.log("Valores dos sliders:", sliderTop, sliderLeft);
+
+$("#top, #left").change(function () {
+    $('#caixa').css({
+        top: 1 + "px",
+        left: 1 + "px"
+    });
+
+    let newTop = $("#top").val();
+    let newLeft = $("#left").val();
+
+
+    
+
+    $("#caixa").css({
+        top: newTop + "px",
+        left: newLeft + "px"
+    });
+    console.log("Posição da caixa:", caixaTop, newTop);
+    console.log("Valores dos sliders:", sliderTop, newLeft);
+});
