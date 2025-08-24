f4668db00516626ceccd02c1de341501fc30f299
Ediley Gomes <ediley.gomes@pucpr.edu.br>
2025-08-24 19:28:22 -0400

    mais uma aula rápida, mas não inutil. O método length é usado para medir a quantidade de itens que tem em determinado local. Totalmente util, finalmente uma aula para pensantes kkk

---
+++ Aula-51-Função-length-em-jQuery/script/script.js
@@ -0,0 +1,12 @@
+$('#btn-lengthp').click(function (e) { 
+    e.preventDefault();
+    
+
+    alert($('p').length);
+});
+
+$('#btn-lengthh4').click(function (e) { 
+    e.preventDefault();
+
+    alert($('h4').length);
+});
